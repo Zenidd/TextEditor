479d00040e34065e20f9ecb795b1ecfd5ba234e2
Zenidd <jpsaezgutierrez@gmail.com>
2019-04-16 10:34:04 +0200

    Added 'q' to quit and turned off echo in shell through use of termios struct and termios.h lib

--- editor.c
+++ editor.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+
+/* TextEditor V0.1 */
+/*
+Commands:
+	q - quit
+
+*/
+
+void enableRawMode() {
+  struct termios raw;
+  tcgetattr(STDIN_FILENO, &raw);
+  raw.c_lflag &= ~(ECHO);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main(){
-	printf("Hello writer \n");	
-	return 0;
+	enableRawMode();
+	char c;
+  	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');	//STDIN_FILENO is a file descriptor, defined in unistd as 0. 
 }
